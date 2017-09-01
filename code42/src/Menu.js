7957a89296b474a48aa9ef99824163be60299f42
Marcus Pope <me@marcuspope.com>
2017-09-01 15:10:37 -0500

    Wired up memebers api to sidebar.  Added logic to handle selected states and UI feedback.

--- code42/src/Menu.js
+++ code42/src/Menu.js
@@ -1,42 +1,115 @@
 import React, { Component } from 'react';
 import Devs from './Devs.js';
+import creds from './creds.json';
 
 class Menu extends Component {
 
     constructor() {
         super();
 
-        //@todo: Populate initial state with members request
-        this.state = {
-            devs: [{
-                id: 1,
-                name: 'Philip J. Fry',
-            }, {
-                id: 2,
-                name: 'Bender'
-            }],
-        };
+        this.state = {};
+
+        //Populate initial state with members request
+        fetch("https://api.github.com/orgs/code42/members", {
+            method: "GET",
+            headers: {
+                'Authorization': 'Basic ' + btoa([creds.user, creds.token].join(":")),
+            }
+        })
+        .then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            throw new Error(res.statusText);
+        })
+        .then((res) => {
+
+            if (!res.length) {
+                throw new Error("No Results");
+            }
+
+            this.setState({
+                devs: res.map((member) => {
+                    return {
+                        id: member.id,
+                        avatar: member.avatar_url,
+                        name: member.login,
+                        profile: member.url,
+                    };
+                })
+            });
+        })
+        .catch((err) => {
+            //@todo: render message in sidebar about API issues
+            console.log("Error: ", err);
+        })
+        .finally(() => {
+            //let ui show busy state before we expand
+            setTimeout(() => {
+
+                global.$('#devMenu span')
+                    .removeClass("glyphicon-repeat")
+                    .addClass('glyphicon-menu-down');
+
+                this.selectItem({
+                    target: global.$('#devMenu')[0]
+                });
+
+                global.$('#userList').collapse('show');
+            }, 500)
+        });
+    }
+
+    selectItem(e) {
+
+        var parent = e.target.parentElement;
+        var cls = parent.classList;
+
+        //only let selection state apply to certain elements
+        var sel = cls.contains('selectable');
+        if (sel) {
+            global.$('.nav li.active').removeClass('active');
+            global.$(parent).addClass('active');
+        }
+
+        //handle dev menu item uniquely
+        var toggle = cls.contains("toggleable");
+        if (toggle) {
+            cls.toggle('expanded');
+            global.$('#devMenu span').toggleClass('glyphicon-menu-up');
+        }
     }
 
     render() {
         return (
             <div className="c42-developers row col-md-2">
-                <ul className="nav">
+                <ul className="nav" onClick={this.selectItem.bind(this)}>
 
-                    <li className="active">
-                        <a href="#home">
+                    <li className="selectable active">
+                        <a href="#home" id="homeMenu">
                             Home
                             <span className="pull-right glyphicon glyphicon-home"></span>
                         </a>
                     </li>
 
-                    {this.state.devs.map((user) => {
-                        return <Devs
-                            name={user.name}
-                            onClick={(e) => this.props.loadProfile(e, user)}
-                            key={user.id}
-                        />;
-                    })}
+                    <li className="toggleable">
+                        <a href="javascript:;" data-toggle="collapse" data-target="#userList" id="devMenu">
+                            Developers
+                            <span className="pull-right glyphicon glyphicon-repeat"></span>
+                        </a>
+                    </li>
+
+                    <div id="userList" className="collapse">
+                        <ul className="nav">
+                            {(this.state.devs || []).map((user) => {
+                                return <Devs
+                                    name={user.name}
+                                    onClick={(e) => this.props.loadProfile(e, user)}
+                                    key={user.id}
+                                />;
+                            })}
+                        </ul>
+                    </div>
                 </ul>
             </div>
         );
--- code42/src/index.js
+++ code42/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import credentials from './creds.json';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import 'whatwg-fetch';
